d0d934f8da2cb326f6baa29261142ba3dc41ce9b
unknown <infinity_michael@yahoo.com>
2020-04-26 14:04:48 -0700

    feat: added dark theme to the project for a blog post

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ThemeChanger from './ThemeChanger';
+import './scss/styles.scss'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Online Shop</h1>
+      <ThemeChanger />
+      <p>lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem0lorem30lorem30lorem30l <br /> orem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30</p>
+      <section>
+        <div className="card">
+          <h2>News Post One</h2>
+          <p>lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem0lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30</p>
+        </div>
+        <div className="card">
+          <h2>News Post Two</h2>
+          <p>lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem0lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30</p>
+        </div>
+        <div className="card">
+          <h2>News Post Three</h2>
+          <p>lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem0lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30lorem30</p>
+        </div>
+      </section>
     </div>
   );
 }
