f0455fa28490659fa13c5a5f6fce59a5272a443e
unknown <infinity_michael@yahoo.com>
2020-05-02 12:31:29 -0700

    feat:  added notifiaction component

--- src/components/advert/Advert.js
+++ src/components/advert/Advert.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import SlideOne from '../../images/slide1 (2).jpg';
-import SlideTwo from '../../images/slide6.jpg';
+// import SlideTwo from '../../images/slide6.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import {
   faFacebook,
   faTwitter,
@@ -9,38 +11,74 @@ import {
 
 } from "@fortawesome/free-brands-svg-icons";
 
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
 const Advert = () => {
   return (
     <div className="advert">
-      <div className="advert-links">
-        <h5>LA More Collectons Showrooms</h5>
-        <p className="mt-1">We bring to you our showroom of latest wears comming soon!!</p>
-        <div className="my-2">
+      <div className="advert-links mt-3">
+        <h5>LA More Collections Showrooms</h5>
+        <p className="mt-1">We bring to you our showroom of all latest wears coming soon!!</p>
+        <div className=" socials my-2">
           <a
             href="https://web.facebook.com/chinweikemichael.chinonso"
             target="_blanck"
+            title="facebook"
           >
-            <FontAwesomeIcon icon={faFacebook} color="brown" size="2x" />
+            <FontAwesomeIcon icon={faFacebook} color="#ee6a65" size="2x" />
           </a>
           <a
             href="https://twitter.com/cmcWebCode?s=07"
             target="_blanck"
+            title="twiiter"
           >
-            <FontAwesomeIcon icon={faTwitter} color="brown" size="2x" />
+            <FontAwesomeIcon icon={faTwitter} color="#ee6a65" size="2x" />
           </a>
           <a
             href="https://github.com/cmcWebCode40"
-            color="brown"
+            color="#ee6a65"
             target="_blanck"
+            title="instagram"
           >
-            <FontAwesomeIcon color="brown" icon={faInstagram} size="2x" />
+            <FontAwesomeIcon color="#ee6a65" icon={faInstagram} size="2x" />
           </a>
         </div>
       </div>
-      <div className="advert-image1"> <img src={SlideOne} height="240" width="300" alt="gallery one" /></div>
-      <div className="advert-image1">
-        <img src={SlideTwo} alt="gallery two" width="300" />
-      </div>
+      <Carousel responsive={responsive}>
+        <div className="advert-image1">
+          <img src={SlideOne} height="300" width="500" alt="gallery one" />
+        </div>
+        <div className="advert-image1">
+          <img src={SlideOne} height="300" width="500" alt="gallery one" />
+        </div>
+        <div className="advert-image1">
+          <img src={SlideOne} height="300" width="500" alt="gallery one" />
+        </div>
+        <div className="advert-image1">
+          <img src={SlideOne} height="300" width="500" alt="gallery one" />
+        </div>
+        <div className="advert-image1">
+          <img src={SlideOne} height="300" width="500" alt="gallery one" />
+        </div>
+      </Carousel>
     </div>
   )
 }
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,16 +1,28 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { context } from '../context/ContextApi';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Notifications from '../notifications/Notifications';
 import imageCart from '../../images/slide3.jpg';
+import CartImage from '../../images/commerce-and-shopping.svg';
 
+const NoCartAvailabe = () => (
+  <div className="no-cart">
+    <div className="no-cart-image mb-2">
+      <img height="150" width="150" src={CartImage} alt="no cart available" />
+    </div>
+    <Link to="/" className="btn btn-light">
+      Start Shopping
+    </Link>
+  </div>
+)
 const Cart = () => {
-  // const [setCart] = useContext(context);
   const [notify, setNotify] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
   const getCart = JSON.parse(localStorage.getItem('cart'));
 
   const increasePerItemQty = (id) => {
     const data = getCart.map(cart => (
-      (cart.id === id) ? { ...cart, count: cart.count + 1 } : cart
+      (cart.id === id) ? { ...cart, count: cart.count + 1, price: Number(cart.price * (cart.count)) } : cart
     ))
     localStorage.setItem('cart', JSON.stringify(data));
     setNotify(!notify)
@@ -27,42 +39,76 @@ const Cart = () => {
       cart.id !== id ? cart : null
     )
     localStorage.setItem('cart', JSON.stringify(data));
-    setNotify(!notify)
+
+    setTimeout(() => {
+      setNotify(!notify)
+    }, 1000);
+    setNotify(false)
   }
 
-  useEffect(() => {
 
-  }, [notify])
+  useEffect(() => {
+    const getTotal = localStorage.getItem('cart');
+    if (getTotal) {
+      const data = getCart.reduce((a, b) => a + Number(b.price), 0)
+      setTotalPrice(data);
+    }
+  }, [getCart])
 
   return (
     <div className="cart">
-      <h3 className="mx-1 ">MY CAART ({getCart ? getCart.length : 0} ITEMS)</h3>
-      {(getCart) ? getCart.map(cart => (
-        <div className="cart-box mx-1 py-1 my-2" key={cart.id}>
-          <div className="cart-box-grid">
-            <img src={imageCart} alt={cart.title} />
-            <div className="cart-box-grid-text">
-              <p>{cart.title}</p>
-              <span>{cart.price}</span>
+      <h3 className="mx-1 ">MY CART ({getCart ? getCart.length : 0} ITEMS)</h3>
+      {notify && <Notifications message='Item removed from cart' classStyle='new' />}
+      <div className="cart-grid">
+        {(getCart.length) ? getCart.map(cart => (
+          <div className="cart-box m-2 py-1 " key={cart.id}>
+            <div className="cart-box-grid">
+              <img src={imageCart} alt={cart.title} />
+              <div className="cart-box-grid-text">
+                <p>{cart.title}</p>
+                <span>{cart.price}</span>
+              </div>
             </div>
-          </div>
-          <div className="cart-box-bottom">
-            <span onClick={() => deleteFromCart(cart.id)}>
-              <FontAwesomeIcon
-                icon="trash"
+            <div className="cart-box-bottom">
+              <span onClick={() => deleteFromCart(cart.id)}>
+                <FontAwesomeIcon
+                  icon="trash"
+                  size="1x"
+                  style={{ marginRight: "1rem" }}
+                  color="#ee6a65"
+                />
+             Remove
+            </span>
+              <span onClick={() => decreasePerItemQty(cart.id)}>  <FontAwesomeIcon
+                icon="minus-circle"
                 size="1x"
                 style={{ marginRight: "1rem" }}
-                color="brown"
-              />
-             delete
-            </span>
-            <span onClick={() => decreasePerItemQty(cart.id)}>-</span>
-            <span>{cart.count}</span>
-            <span onClick={() => increasePerItemQty(cart.id)}>+</span>
+                color="#ee6a65"
+              /></span>
+              <span>{cart.count}</span>
+              <span onClick={() => increasePerItemQty(cart.id)}>  <FontAwesomeIcon
+                icon="plus-circle"
+                size="1x"
+                style={{ marginRight: "1rem" }}
+                color="#ee6a65"
+              /></span>
+            </div>
           </div>
-        </div>
-      )) : <p>NO CART</p>}
-      <p className="total">Total <span>:bla bla bla </span> <br /> <a href="www.call.com">Call To Order</a></p>
+
+        )) : <NoCartAvailabe />}
+      </div>
+      <p className="total mb-1">Total <span>{totalPrice ? totalPrice : 0} </span></p>
+      <br />
+      <div className="mb-3 cart-btn">
+        <a href="www.call.com" className="btn">
+          <FontAwesomeIcon
+            icon="phone-alt"
+            size="1x"
+            style={{ marginRight: '1rem ' }}
+            color="#fff"
+          />
+      Call To Order</a>
+      </div>
     </div>
   )
 };
