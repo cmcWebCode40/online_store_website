4bf94ff398f9e969968cfb46d1630b3a4d164246
unknown <infinity_michael@yahoo.com>
2020-05-19 14:59:21 -0700

    used workbox for PWA BREAKING CHANGE

--- src/sw.js
+++ src/sw.js
@@ -1,32 +1,24 @@
 /* eslint-disable */
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('workbox-v5.1.3/workbox-sw.js');
 
 workbox.setConfig({
-	modulePathPrefix: 'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+	modulePathPrefix: 'workbox-v5.1.3/'
 });
 
-const precacheManifest = [];
-
 workbox.precaching.suppressingWarnings();
-workbox.precaching.precacheAndRoute(precacheManifest);
+workbox.precaching.precacheAndRoute([]);
 
 const dataCacheConfig = {
 	cacheName: 'online-shop'
 };
-
 workbox.routing.registerRoute(
-	'https://online-shop-api.herokuapp.com/bags/',
-	workbox.strategies.cacheFirst(dataCacheConfig),
-	'GET'
+	new RegExp('https://online-shop-api.herokuapp.com/bags/'),
+	workbox.strategies.cacheFirst(dataCacheConfig)
 );
 workbox.routing.registerRoute(
-	'https://online-shop-api.herokuapp.com/bags/',
-	workbox.strategies.staleWhileRevalidate(dataCacheConfig),
-	'GET'
+	new RegExp('https://online-shop-api.herokuapp.com/shoes/'),
+	workbox.strategies.cacheFirst(dataCacheConfig)
 );
-
-workbox.routing.registerRoute('https://online-shop-api.herokuapp.com/shoes/');
 workbox.routing.registerRoute(
 	/.*.(?:png|jpg|jpeg|svg)$/,
 	workbox.strategies.cacheFirst({
